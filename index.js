7a0e81d1bfdc23e5280cdb62b31f2e4a99a552be
Habibur137 <infernohabib@gmail.com>
2022-05-22 22:19:34 +0600

    connect with mongodb

--- index.js
+++ index.js
@@ -7,6 +7,23 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.onllh.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+    console.log("mongodb connect");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
